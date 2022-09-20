0b25513d8f6a24514c0d608a7a39b36773214373
Fung Tsui <harry072228@gmail.com>
2022-09-20 17:20:28 +0800

    add the  detail page

--- FRD/src/pages/product/Details/[id].tsx
+++ FRD/src/pages/product/Details/[id].tsx
@@ -5,47 +5,42 @@ import { Heading } from '../../../components/Heading'
 import { Footer } from '../../../components/Footer'
 import { DetailBox2 } from '../../../components/detailBox2'
 import { useEffect, useState } from 'react'
-import {  useRouter } from 'next/router'
-
+import { useRouter } from 'next/router'
 
 interface product {
-		id: number,
-		name: string,
-		icon: string,
-		image1: string,
-		image2: string,
-		image3: string,
-		Brand: string,
-	}
-
+	id: number
+	name: string
+	icon: string
+	image1: string
+	image2: string
+	image3: string
+	Brand: string
+}
 
 const ProductDetails: NextPage = () => {
-
 	const router = useRouter()
-	const { id } = router.query
-	
-	const [product, setProduct] = useState<product []>([])
+	// const { id } = router.query
 
-	async function fetchProduct() {
-		let res = await fetch(`${process.env.NEXT_PUBLIC_ANALYTICS_ID}/productDetailInfo/${id}`
+	const [product, setProduct] = useState<product[]>([])
+
+	async function fetchProduct(id: number) {
+		let res = await fetch(
+			`${process.env.NEXT_PUBLIC_ANALYTICS_ID}/productDetailInfo/${id}`
 		)
-		let product = ((await res.json()).productInfo).productInfo
+		let product = (await res.json()).productInfo.productInfo
 		setProduct(product)
 
 		console.log(product)
 	}
 
-	useEffect(()=>{
-		
-		if(router.isReady){
-			const { id } = router.query
-			console.log(id);
-			
-			fetchProduct()
+	useEffect(() => {
+		if (router.isReady) {
+			const query = router.query.id
+			const id = Number(query)
+
+			fetchProduct(id)
 		}
-	},[setProduct,[router.isReady]])
-
-
+	}, [setProduct, router.isReady])
 
 	return (
 		<>
@@ -53,15 +48,15 @@ const ProductDetails: NextPage = () => {
 			<Heading />
 			<Navbar />
 			{product.map((product) => (
-			<DetailBox2
-				id={product.id}
-				name={product.name}
-				icon={product.icon}
-				image1={product.image1}
-				image2={product.image2}
-				image3={product.image3}
-				Brand={product.Brand}
-			/>
+				<DetailBox2
+					id={product.id}
+					name={product.name}
+					icon={product.icon}
+					image1={product.image1}
+					image2={product.image2}
+					image3={product.image3}
+					Brand={product.Brand}
+				/>
 			))}
 
 			<Footer />
