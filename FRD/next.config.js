1827838aacdb08df101de1d5a2ea6dc7fca70377
charleskan <71898304+charleskan@users.noreply.github.com>
2022-09-27 21:58:33 +0800

    before deployment 2

--- FRD/next.config.js
+++ FRD/next.config.js
@@ -3,7 +3,16 @@ const nextConfig = {
 	reactStrictMode: false,
 	swcMinify: false,
 	images: {
-		domains: ['hibuysomemall.me'] 
+		domains: [
+		'https://api.hibuysomemall.me/uploads', 
+		'https://www.hibuysomemall.me',
+		'https://hibuysomemall.me/userUploadedFiles',
+		'hibuysomemall.me/userUploadedFiles',
+		'api.hibuysomemall.me/uploads',
+		'https://hibuysomemall.me',
+		'hibuysomemall.me',
+	
+	] 
 
 	}
 }
