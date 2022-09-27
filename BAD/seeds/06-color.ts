934107b4c62269ed4662eb890bc3968b66c2c538
charleskan <71898304+charleskan@users.noreply.github.com>
2022-09-27 16:28:10 +0800

    fixed Seeds bugs

--- BAD/seeds/06-color.ts
+++ BAD/seeds/06-color.ts
@@ -1,18 +1,39 @@
 import { Knex } from "knex";
-import { faker } from '@faker-js/faker';
+
 
 export async function seed(knex: Knex): Promise<void> {
     // Deletes ALL existing entries
-    faker.seed(123);
+    
 
     await knex("color").del();
 
     // Inserts seed entries
-    for (let i = 0; i < 7; i++) {
-        await knex.insert({
-            name: faker.color.human(),
-        }).into("color");
-    }
+    await knex
+
+    .insert([
+        { 
+            name: "Red",
+        },
+        {
+            name: "Blue",
+        },
+        {
+            name: "Green",
+        },
+        {
+            name: "Black",
+        },
+        {
+            name: "White",
+        },
+        {
+            name: "Purple",
+        }
+
+
+
+    ])
+        .into("color")
 
 
 }
